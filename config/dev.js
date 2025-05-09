6c91eb4b6247ff288c6c4ef470c2f8068be76b5e
kennethdb25 <80439952+kennethdb25@users.noreply.github.com>
2025-05-09 19:40:17 +0800

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/ogims",
-  // mongoURI: 'mongodb+srv://eparam-user:Password10@e-param.pepaxce.mongodb.net/OGIMS?retryWrites=true&w=majority',÷
+  // mongoURI: "mongodb://localhost:27017/ogims",
+  mongoURI: 'mongodb+srv://eparam-user:Password10@e-param.pepaxce.mongodb.net/OGIMS?retryWrites=true&w=majority',÷
   cookieKey: "qowjepqjwo1021823018asdjlkasj",
 };
